363eccf051ceba3a213319de23a53a0b76ee182a
William Born <wkb1997@gmail.com>
2019-03-19 15:41:34 +0000

    started fixing constants for sick lidar

--- src/drive_jackal.py
+++ src/drive_jackal.py
@@ -28,27 +28,27 @@ def state():
 		move_cmd.angular.z=0
 	if command == 'cw':
 		move_cmd.linear.x=0
-		move_cmd.angular.z=-.5
+		move_cmd.angular.z=.5
 	if command == 'ccw':
 		move_cmd.linear.x=0
-		move_cmd.angular.z=.5
+		move_cmd.angular.z=-.5
 	if command == 'cwf':
 		move_cmd.linear.x=0
-		move_cmd.angular.z=-1
+		move_cmd.angular.z=1
 	if command == 'ccwf':
 		move_cmd.linear.x=0
-		move_cmd.angular.z=1
+		move_cmd.angular.z=-1
 	if command == 'f':
-		move_cmd.linear.x=.2
+		move_cmd.linear.x=.4
 		move_cmd.angular.z=0
 	if command == 'ff':
-		move_cmd.linear.x=.4
+		move_cmd.linear.x=.9
 		move_cmd.angular.z=0
 	if command == 'r':
 			move_cmd.linear.x=-.1
 			move_cmd.angular.z=0
 	cmd_vel.publish(move_cmd)
-	rospy.loginfo("I published "+str(move_cmd))
+	#rospy.loginfo("I published "+str(move_cmd))
 		
 
 def drive():
--- src/lidar.py
+++ src/lidar.py
@@ -11,7 +11,9 @@ urg_list = []
 
 def urg_callback(data):
 	global urg_list
-	urg_list = data.ranges
+	urg_list = list(data.ranges)
+	#for i in range(0,len(urg_list)):
+	#	urg_list[i] = urg_list[i]
 
 def mode_callback(data):
 	global mode
@@ -22,29 +24,39 @@ def loop():
 	global sign, angle
 
 	#print urg_list
-	
-	pw=.47 #width of the robot	
-		
+
+	pw=1.3 #width of the robot
+
 	ranges=[] #initialize array to store maximum distance that
 			  #robot can travel at each beam
 	beam_cut=1
-	if len(urg_list)>0: #only run once a beam array has been recieved	
-		for i in range(0,len(urg_list),beam_cut):		
+
+	#rospy.loginfo(len(urg_list))
+	#rospy.loginfo(urg_list)
+	if len(urg_list)>0: #only run once a beam array has been recieved
+		num_beams = len(urg_list)
+		for i in range(0,len(urg_list),beam_cut):
 			ran =  urg_list[i] #range for path checking
+			if ran==float("Inf"):
+				ran=15.0
+			if ran==float("NaN"):
+				ran=0.0
+			if ran<0.01:
+				ran=.05
 			max_dist = urg_list[i] #this variable will be used to store the
 								   #max distance that the robot can travel
 			check = True
 			for k in range (0,20):
-				if check: 				
+				if check:
 					#how many degrees should be checked for the given range
-					ran2= k * (ran/20)			
+					ran2= k * (ran/20)
 					width = 2 * numpy.degrees(numpy.arctan(pw/2))/ran2
-					width = (width * 4)/ 2 #resolution of hokuyo is .25 degrees, so
+					width = 3*(width)/ 2 #resolution of hokuyo is .25 degrees, so
 										   #there are four array elements per degree
-							
+
 					lower_width=width #differentiate width for array bounds checking
-					if (i+width)>1080:
-						width = 1080-i #if the width will exceed the max of the array
+					if (i+width)>num_beams:
+						width = num_beams-i #if the width will exceed the max of the array
 									   #then only check until the end of the array
 					if (i-lower_width)<0:
 						lower_width=i  #same concept for lower bound of array
@@ -52,63 +64,59 @@ def loop():
 					for j in range (i-int(lower_width),i+int(width)):
 						if urg_list[j]<ran2:
 							max_dist= urg_list[j]
-							check=False		
+							if(max_dist>.1):
+								check=False
 					#add maximum distance the robot can travel for this beam to array
 			ranges.append(max_dist)
 
 		global drive
 
 		#rospy.loginfo(str(ranges))
-		
+
 		#if the most open path is outside of 16 degrees in front of robot, steer
-		angle=3.1416
-		if ranges[((1080/2)/beam_cut)]>2.3:
-			drive.publish('ff')
-			angle=3.2		
-		elif ranges[((1080/2)/beam_cut)]>1.5:
-			drive.publish('f')
-			angle=3.3
-		elif max(ranges)<.5:
-			drive.publish('cwf')
-		elif ranges.index(max(ranges)) < 0+((1080/beam_cut)/3):
-			drive.publish('cwf')
-		elif ranges.index(max(ranges)) < ((1080/2)/beam_cut)-((5*4)/beam_cut):
-			drive.publish('cw')
-		elif ranges.index(max(ranges)) > ((1080/2)/beam_cut)+((5*4)/beam_cut):
-			drive.publish('ccw')
-		elif ranges.index(max(ranges)) > (1080/beam_cut)-(1080/beam_cut)/3:
-			drive.publish('ccwf')
-		else:
-			drive.publish('f')	
-		
-		hok_ang.publish(angle)
+
+		if(ranges[int((num_beams/2)/beam_cut)]>.01):
+			#rospy.loginfo(num_beams)
+			if ranges[int((num_beams/2)/beam_cut)]>2:
+				drive.publish('ff')
+				angle=3.2
+			elif ranges[int((num_beams/2)/beam_cut)]>1.0:
+				drive.publish('f')
+				angle=3.3
+			elif max(ranges)<.5:
+				drive.publish('cwf')
+			elif ranges.index(max(ranges)) < 0+((num_beams/beam_cut)/3):
+				drive.publish('cwf')
+			elif ranges.index(max(ranges)) < ((num_beams/2)/beam_cut)-((10)/beam_cut):
+				drive.publish('cw')
+			elif ranges.index(max(ranges)) > ((num_beams/2)/beam_cut)+((10)/beam_cut):
+				drive.publish('ccw')
+			elif ranges.index(max(ranges)) > (num_beams/beam_cut)-(num_beams/beam_cut)/3:
+				drive.publish('ccwf')
+			else:
+				drive.publish('f')
+
+
 #plt.plot([i*.25+width/2,i*.25-width/2],[ran,ran],'b-')
-		
+
 def main():
 	rospy.init_node("lidar")
 
-	rate = rospy.Rate(30)	
-	
+	rate = rospy.Rate(30)
+
 	rospy.Subscriber("/scan", LaserScan, urg_callback)
-	rospy.Subscriber("/out/mode",String, mode_callback)	
+	rospy.Subscriber("/out/mode",String, mode_callback)
 
 	global drive
 
 	drive = rospy.Publisher("/out/drive",String)
-	
-	global hok_ang, sign, angle
-	hok_ang = rospy.Publisher("/hokuyo_motor/command", Float64)
-	
-	hok_ang.publish("3.1416")
-	sign = 1
-	angle = 3.1416
 
 	#plt.show()
 	#plt.pause(0.0001)
 	global mode
 	mode='stop'
-	while not rospy.is_shutdown():		
-		if mode == 'lidar':		
+	while not rospy.is_shutdown():
+		if mode == 'lidar':
 			loop()
 		rate.sleep()
 
